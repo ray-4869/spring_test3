08e44cdc5f1198160d69c3b482c921eeaee8f65f
ray-4869 <rlfgustn777@gmail.com>
2024-09-23 10:45:38 +0900

    detail

--- src/main/java/com/sbb/sbb/article/ArticleController.java
+++ src/main/java/com/sbb/sbb/article/ArticleController.java
@@ -1,9 +1,13 @@
 package com.sbb.sbb.article;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -12,10 +16,33 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 public class ArticleController {
-    private ArticleService articleService;
+    private final ArticleService articleService;
     @GetMapping("/list")
-    public String list (Model model){
+    public String articleList (Model model){
         List<Article> list = this.articleService.getList();
+        model.addAttribute("articleList", list);
         return "article_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String articleDetail (Model model, @PathVariable("id") Integer id) {
+        Article article = this.articleService.getArticle(id);
+        model.addAttribute("article", article);
+        return "article_detail";
+    }
+
+    @GetMapping("/create")
+    public String articleCreate(ArticleForm articleForm) {
+        return "article_form";
+    }
+
+    @PostMapping("/create")
+    public String articleCreate(@Valid ArticleForm articleForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "aritcle_form";
+        }
+        this.articleService.create(articleForm.getTitle(), articleForm.getContent());
+        return "redirect:/article/list";
+
     }
 }
--- src/main/java/com/sbb/sbb/article/ArticleService.java
+++ src/main/java/com/sbb/sbb/article/ArticleService.java
@@ -1,16 +1,37 @@
 package com.sbb.sbb.article;
 
+import com.sbb.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+
+import static java.time.LocalDateTime.*;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
-    private ArticleRepository articleRepository;
+    private final ArticleRepository articleRepository;
 
     public List<Article> getList() {
         return this.articleRepository.findAll();
+    }
+    public Article getArticle (Integer id) {
+        Optional<Article> article = this.articleRepository.findById(id);
+        if(article.isPresent()){
+            return article.get();
+        }else {
+            throw new DataNotFoundException("article not found");
+        }
+    }
+
+    public void create (String title, String content) {
+        Article ca = new Article();
+        ca.setTitle(title);
+        ca.setContent(content);
+        ca.setCreateDate(LocalDateTime.now());
+        this.articleRepository.save(ca);
     }
 }
